22b95101410f3b32ae7a1c0d5046e9d81a1bbed2
Zain Abro <zainabro886@gmail.com>
2025-04-15 22:23:28 +0500

    Removing Portfolio

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -23,7 +23,6 @@ const Home = () => {
       <Hero content={aboutData} isLoading={isLoading} />
       <About content={aboutData} isLoading={isLoading} />
       <Projects />
-      <Portfolio />
       <Testimonial />
       <HappyClients />
       <Footer footerText={"Have a Project?"} text={"Let's Talk"} />
